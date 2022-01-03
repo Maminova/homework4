fb1e5186630c1ff931d1dded5843aa7fb0c4bf95
Zabira <z.maminova@bk.ru>
2022-01-03 13:38:45 +0300

    Задание 4, исправленная версия

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -12,25 +12,28 @@ public class Main {
         }
         System.out.println();
 
-        for (int countdown = 10; countdown > 0; countdown --) {
-            System.out.print("" + countdown);
+        for (int countDown = 10; countDown >= 1; countDown--) {
+            System.out.print("" + countDown);
         }
         System.out.println();
         // Задание 2
-
-        for (int j = 1; j <= 31; j = j+7){
-            System.out.println("Сегодня отчет:" + j +"-ое число. Необходимо подготовить отчет");
-}
-            // Задание 3
-            int year = 2022;
-            int yearBefore = year - 200;
-            int yearAfter = year + 100;
-            for (year = yearBefore; year < yearAfter; year = year + 79){
-                System.out.println(year);
+        int j = 1;
+        for (; j <= 31; j = j + 7) {
+            System.out.println("Сегодня отчет:" + j + "-ое число. Необходимо подготовить отчет");
+        }
+        // Задание 3
+        int yearNow = 2022;
+        int yearBefore = yearNow - 200;
+        int yearAfter = yearNow + 100;
+        for (yearNow = yearBefore; yearNow < yearAfter; yearNow++)
+            if (yearNow % 79 == 0) {
+                System.out.println(yearNow);
             }
-        }
+    }
+}
 
-        }
+
+
 
 
 
